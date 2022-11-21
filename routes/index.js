2a14ca3269f62680104d0e247121e3c600d89c3a
Sergey1759 <grt.sergey.lisitskii@gamil.com>
2022-11-21 15:31:06 +0100

    nothing important

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/profile', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('profile', { title: 'Express' });
 });
 
 module.exports = router;
